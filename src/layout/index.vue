f0e52fa48610ae4c3f9ee2392e5059db689d368c
zhanglibo <zhanglibo@leifeng-elec.com>
2020-12-17 16:15:33 +0800

    基础模板

---
+++ src/layout/index.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-12-17 16:07:52
+ * @LastEditTime: 2020-12-17 16:08:25
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vuemould\src\layout\index.vue
+-->
+<template>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
